158c1ee12e8d60eb356ee45d053b1ed1d46f6473
saheersk <saheerabcd3@gmail.com>
2023-11-23 12:21:19 +0530

    Kubernetes file added

--- fashion/asgi.py
+++ fashion/asgi.py
@@ -1,12 +1,3 @@
-"""
-ASGI config for fashion project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
-"""
-
 import os
 import django
 
